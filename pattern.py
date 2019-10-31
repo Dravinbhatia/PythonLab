bee1596b23dfee64b61f0461c0965099ceaaba55
Dravinbhatia <dravinbhatia1997@gmail.com>
2019-10-31 22:46:23 +0530

    Loop variable

--- pattern.py
+++ pattern.py
@@ -1,9 +1,9 @@
 n = 5
-for i in range(1, n):
-    for j in range(1, i+1):
+for k in range(1, n):
+    for j in range(1, k+1):
         print(j),
     print('')
-for i in range(n-1,0, -1):
-    for j in range(1, i+1):
+for k in range(n-1,0, -1):
+    for j in range(1, k+1):
         print(j),
     print('')
